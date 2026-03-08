cd322cd6688aaaaee5e93e48364fb9cc7bf20151
KingsDevOfc <carlosdanieldutra2k24@gmail.com>
2026-03-08 04:25:29 -0300

    Add animated hearts that appear on red background

---
+++ heart.js
@@ -0,0 +1,106 @@
+setInterval(()=>{
+
+if(document.body.classList.contains("red")){
+
+let heart = document.createElement("div")
+
+heart.innerHTML="🖤"
+
+heart.style.position="fixed"
+heart.style.left=Math.random()*100+"%"
+heart.style.bottom="0"
+
+heart.style.fontSize="25px"
+
+document.body.appendChild(heart)
+
+let pos=0
+
+let anim=setInterval(()=>{
+
+pos+=2
+
+heart.style.bottom=pos+"px"
+
+if(pos>800){
+
+heart.remove()
+clearInterval(anim)
+
+}
+
+},20)
+
+}
+
+},600)setInterval(()=>{
+
+if(document.body.classList.contains("red")){
+
+let heart = document.createElement("div")
+
+heart.innerHTML="🖤"
+
+heart.style.position="fixed"
+heart.style.left=Math.random()*100+"%"
+heart.style.bottom="0"
+
+heart.style.fontSize="25px"
+
+document.body.appendChild(heart)
+
+let pos=0
+
+let anim=setInterval(()=>{
+
+pos+=2
+
+heart.style.bottom=pos+"px"
+
+if(pos>800){
+
+heart.remove()
+clearInterval(anim)
+
+}
+
+},20)
+
+}
+
+},600)setInterval(()=>{
+
+if(document.body.classList.contains("red")){
+
+let heart = document.createElement("div")
+
+heart.innerHTML="🖤"
+
+heart.style.position="fixed"
+heart.style.left=Math.random()*100+"%"
+heart.style.bottom="0"
+
+heart.style.fontSize="25px"
+
+document.body.appendChild(heart)
+
+let pos=0
+
+let anim=setInterval(()=>{
+
+pos+=2
+
+heart.style.bottom=pos+"px"
+
+if(pos>800){
+
+heart.remove()
+clearInterval(anim)
+
+}
+
+},20)
+
+}
+
+},600)
