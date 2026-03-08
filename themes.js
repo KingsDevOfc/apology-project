6b5852af4e0a3ae84ca511d50123cf243b153902
KingsDevOfc <carlosdanieldutra2k24@gmail.com>
2026-03-08 04:39:33 -0300

    Add toggleTheme function for theme switching

---
+++ themes.js
@@ -0,0 +1,29 @@
+function toggleTheme(){
+
+if(document.body.classList.contains("goth")){
+
+document.body.classList.remove("goth")
+document.body.classList.add("red")
+
+}else{
+
+document.body.classList.remove("red")
+document.body.classList.add("goth")
+
+}
+
+}function toggleTheme(){
+
+if(document.body.classList.contains("goth")){
+
+document.body.classList.remove("goth")
+document.body.classList.add("red")
+
+}else{
+
+document.body.classList.remove("red")
+document.body.classList.add("goth")
+
+}
+
+}
